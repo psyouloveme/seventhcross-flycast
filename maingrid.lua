1dc39a491f9825c53144c53b5ccd183021f2e4bc
psyouloveme <4607641+psyouloveme@users.noreply.github.com>
2024-04-23 14:31:11 -0700

    Finish maingrid module doc

--- maingrid.lua
+++ maingrid.lua
@@ -1,25 +1,42 @@
 ---Grid operations
----@module "lua.seventhcross.maingrid"
 local consts = require("lua.seventhcross.constants")
 local memory = require("lua.seventhcross.memory")
+
+---@class Coord2 (x, y) coordinate
+---@field x integer x coordinate.
+---@field y integer y coordinate.
+
+---@class GridChange a result from the comparison of grids A and B
+---@field a number the value read from grid A
+---@field b number the value read from grid B
+---@field x integer the x coordinate that the value was read from (in both grids)
+---@field y integer the y coordinate that the value was read from (in both grids)
+
+---@alias Grid number[][] a grid of floating point values
+---@alias GridChangeCollection GridChange[] an array of grid changes
+
+---@class GridParseResult stringify and compare result
+---@field grid_string string the string represenation of the left hand side input grid
+---@field changes GridChangeCollection changes between grids a and b
 
 --[[
     The main 200x200 evolution grid that is sub-divided
     into 40 10x10 grids that correspond to positions in the
     20x20 super-grid.
 ]]
+---@module "lua.seventhcross.maingrid"
 local grid = {
-
     --[[
         The main 200x200 evolution grid that is sub-divided
         into 40 10x10 grids that correspond to positions in the
         20x20 super-grid.
     ]]
     main = {
+        ---Conversion methods for main grid indices
         index = {
             ---Translate a value index in the main grid to a coordinate in the sub-grid that contains it.
             ---@param index integer index into the array representation of the main grid.
-            ---@return {x: integer, y: integer} coordinate x,y coordinate of the sub-grid that contains this index.
+            ---@return Coord2 coordinate x,y coordinate of the sub-grid that contains this index.
             to_sub_coord = function(index)
                 return {
                     x = (index % consts.grid.main.width) % consts.grid.sub.width;
@@ -30,7 +47,7 @@ local grid = {
             ---Translate an index into the array representation of the main grid into a super-grid 
             ---matrix coordinate based on the sub-grid that contains the index.
             ---@param index integer index into the array representation of the main grid.
-            ---@return { x: integer, y: integer } coordinate x,y coordinate of the super-grid that contains this index.
+            ---@return Coord2 coordinate x,y coordinate of the super-grid that contains this index.
             to_super_coord = function(index)
                 return {
                     x = (index % consts.grid.main.width) % consts.grid.super.width;
@@ -40,7 +57,7 @@ local grid = {
 
             ---Translate a value index in the main grid to the corresponding coordinate in the matrix representation of the main grid
             ---@param index integer index into the array representation of the main grid.
-            ---@return {x: integer, y: integer} coordinate the main-grid coordinate that corresponds to index.
+            ---@return Coord2 coordinate the main-grid coordinate that corresponds to index.
             to_coord = function(index)
                 return {
                     x = index % consts.grid.main.width;
@@ -56,6 +73,7 @@ local grid = {
             end;
         };
 
+        ---Conversion methods for main grid coordinates
         coord = {
             ---Translate a coordinate from the matrix representation of the main grid into the corresponding index into the array representation of the main grid.
             ---@param x integer main grid matrix representation x coordinate
@@ -77,6 +95,10 @@ local grid = {
                 return (((y * consts.grid.main.width) + x) * consts.grid.main.value_size) + consts.grid.main.start_address
             end;
 
+            ---Translate an x,y coordinate in the 200x200 main grid to the corresponding coordinate in the 20x20 super grid.
+            ---@param x integer the x component to translate
+            ---@param y integer the y component to translate
+            ---@return Coord2 coordinate the corresponding (x,y) coordinate in the 20x20 super grid 
             to_super_coord = function(x,y)
                 local index = (y * consts.grid.main.width) + x
                 return {
@@ -86,6 +108,7 @@ local grid = {
             end;
         };
 
+        ---Conversion methods for main grid offsets
         offset = {
             ---Translate a memory offset into an index into the array representation of the main grid.
             ---@param offset integer memory offset to translate.
@@ -96,7 +119,7 @@ local grid = {
 
             ---Translate a memory offset into a coordinate into the matrix representation of the main grid.
             ---@param offset integer memory offset to translate
-            ---@return { x: integer, y: integer } coordinate coordinate in the matrix representation of the main grid that corresponds to offset.
+            ---@return Coord2 coordinate coordinate in the matrix representation of the main grid that corresponds to offset.
             to_coord = function(offset)
                 return {
                     x = math.floor((offset - consts.grid.main.start_address) / consts.grid.main.value_size) % consts.grid.main.width;
@@ -107,7 +130,7 @@ local grid = {
             ---Translate an index into the array representation of the main grid into a super-grid 
             ---matrix coordinate based on the sub-grid that contains the index.
             ---@param offset integer index into the array representation of the main grid.
-            ---@return { x: integer, y: integer } coordinate x,y coordinate of the super-grid that contains this index.
+            ---@return Coord2 coordinate x,y coordinate of the super-grid that contains this index.
             to_super_coord = function(offset)
                 local index = (offset - consts.grid.main.start_address) / consts.grid.main.value_size
                 return {
@@ -126,6 +149,7 @@ local grid = {
         main 200x200 grid.
     ]]
     super = {
+        ---Conversion methods for super grid coordinates
         coord = {
             ---Translate a super-grid coordinate into the index of the first element of the array representation of the main grid.
             ---@param x integer x coordinate of the matrix representation of the super-grid to translate.
@@ -149,7 +173,7 @@ local grid = {
             ---Translate a super-grid coordinate into main grid matrix coordinate that corresponds to (0,0) of the corresponding sub-grid pointed to by (x,y).
             ---@param x integer x coordinate of the matrix representation of the super-grid to translate.
             ---@param y integer y coordinate of the matrix representation of the super-grid to translate.
-            ---@return { x: integer, y: integer } coordinate main grid matrix coordinate of the (0,0) corresponding sub-grid pointed to by (x,y).
+            ---@return Coord2 coordinate main grid matrix coordinate of the (0,0) corresponding sub-grid pointed to by (x,y).
             to_main_coord = function(x, y)
                 return {
                     x = x * consts.grid.sub.width;
@@ -157,10 +181,15 @@ local grid = {
                 }
             end;
 
+            ---Translate a 20x20 super-grid coordinate to an index into the array represenation of the super-grid.
+            ---@param x integer x coordinate of the matrix representation of the super-grid to translate.
+            ---@param y integer y coordinate of the matrix representation of the super-grid to translate.
+            ---@return integer index the corresponding array index of the coordinate
             to_index = function(x,y)
                 return (y * consts.grid.super.width) + x;
             end;
         };
+        ---Conversion methods for super grid indices
         index = {
             to_coord = function(index)
                 return {
@@ -176,6 +205,7 @@ local grid = {
         that correspond to cells in the 20x20 super-grid.
     ]]
     sub = {
+        ---Conversion methods for sub-grid coordinates
         coord = {
             ---Translate a sub-grid x,y coordinate into a value index into the array representation of the sub-grid that contains it
             ---@param x integer x cooridnate of the coordinate in the matrix representation of this sub-grid
@@ -190,7 +220,13 @@ local grid = {
             end;
         };
     };
+
+    ---Compare two matricies of the same size and return an object containing the differences
+    ---@param a Grid grid to use for left hand side of comparison
+    ---@param b Grid grid to use for right hand side of comparison
+    ---@return GridChangeCollection changes array of changes determined from the grid
     compare = function (a, b)
+        ---@type GridChangeCollection
         local changes = {}
         for y = 0, #a do
             for x = 0, #a[0] do
@@ -207,6 +243,9 @@ local grid = {
         return changes
     end;
 
+    ---Create a string representation of a grid
+    ---@param grid Grid the grid to stringify
+    ---@return string grid_string the string represenation of the grid
     to_string = function (grid)
         local st = "\t\t\t"
         for a = 0, #grid[0] do
@@ -229,6 +268,11 @@ local grid = {
         end
         return st
     end;
+
+    ---Create a string representation of a grid with a specific float precision
+    ---@param grid Grid the grid to stringify
+    ---@param precision integer float precision to pass to string.format
+    ---@return string grid_string the string represenation of the grid
     to_string_p = function (grid, precision)
         local st = "    \t"
         local fmt = "%0."..tostring(precision).."f"
@@ -266,6 +310,12 @@ local grid = {
         end
         return st
     end;
+
+    ---Create a string representation of a grid with a specific float precision and collect changes
+    ---@param grid_a Grid the grid to stringify and the left hand side of the grid comparison
+    ---@param grid_b Grid right hand side of the grid comparison
+    ---@param precision integer float precision to pass to string.format
+    ---@return GridParseResult result the stringified version of grid A and the comparison result vs B
     to_string_p_comp = function (grid_a, grid_b, precision)
         local st = "    \t"
         local fmt = "%0."..tostring(precision).."f"
@@ -307,6 +357,8 @@ local grid = {
             changes = changes;
         }
     end;
+
+    ---Test methods for the grid
     tests = {};
 };
 
@@ -337,7 +389,7 @@ end
 ---Read a 10x10 subgrid from the matrix representation of the main grid by the corresponding super-grid coordinate.
 ---@param x integer super-grid matrix representation x coordinate.
 ---@param y integer super-grid matrix representation y coordinate.
----@return number[][] subgrid a 10x10 matrix of floats that correspond the super-grid coordinate (x,y).
+---@return Grid subgrid a 10x10 matrix of floats that correspond the super-grid coordinate (x,y).
 function grid.sub.read_by_super_coord(x,y)
     local g = {}
     for k = 0, consts.grid.sub.width - 1 do
@@ -354,6 +406,10 @@ function grid.sub.read_by_super_coord(x,y)
     return g
 end
 
+---Compute the "score" of a 10x10 sub-grid by super-grid coordinates
+---@param x integer super-grid matrix representation x coordinate.
+---@param y integer super-grid matrix representation y coordinate.
+---@return number score the square sum of the component values
 function grid.sub.score_by_super_coord(x,y)
     local score = 0
     for k = 0, consts.grid.sub.width - 1 do
@@ -369,6 +425,8 @@ function grid.sub.score_by_super_coord(x,y)
     return score
 end
 
+---Create a 20x20 grid of floating point score values corresponding to the super-grid
+---@return Grid subgrid a 10x10 matrix of floats that correspond the super-grid coordinate (x,y).
 function grid.super.get_score_grid()
     local g = {}
     for k = 0, consts.grid.super.width - 1 do
@@ -388,7 +446,7 @@ end
 ---Read a 10x10 subgrid from the matrix representation of the main grid by the corresponding super-grid coordinate.
 ---@param x integer super-grid matrix representation x coordinate.
 ---@param y integer super-grid matrix representation y coordinate.
----@return number[][] subgrid a 10x10 matrix of floats that correspond the super-grid coordinate (x,y).
+---@return Grid subgrid a 10x10 matrix of floats that correspond the super-grid coordinate (x,y).
 function grid.sub.read_by_super_coord_transposed(x,y)
     local g = {}
     for k = 0, consts.grid.sub.width - 1 do
@@ -406,7 +464,12 @@ function grid.sub.read_by_super_coord_transposed(x,y)
 end
 
 --[[ ********** Tests **********]]
-
+---Unchecked test for several conversion methods
+---@param x integer main-grid coordinate x component
+---@param y integer main-grid coordinate x component
+---@param index integer main-grid array offset
+---@param offset integer main-grid memory offset
+---@return string results test results in string format
 function grid.tests.run_conversions(x, y, index, offset)
     local st = ""
     if x ~= nil and y ~= nil then
@@ -443,6 +506,12 @@ function grid.tests.run_conversions(x, y, index, offset)
     return st
 end
 
+---Test for several conversion methods for equlivalant values
+---@param x integer known main-grid coordinate x component
+---@param y integer known main-grid coordinate x component
+---@param index integer known main-grid array offset
+---@param offset integer known main-grid memory offset
+---@return string results test results in string format
 function grid.tests.test_coords(x, y, index, offset)
     local st = string.format("Coord Test (%d, %d):\n", x, y)
     local i1 = grid.main.coord.to_index(x, y)
